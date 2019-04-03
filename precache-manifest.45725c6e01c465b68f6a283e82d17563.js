3bcfb5b330976f81b56875791fc1c8ab4084292d
Ben Gosney <bengosney@googlemail.com>
2019-04-03 22:10:30 +0100

    Updates

---
+++ precache-manifest.45725c6e01c465b68f6a283e82d17563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30f6b5daf8dda01c6103",
+    "url": "/dots/static/css/main.2273f12d.chunk.css"
+  },
+  {
+    "revision": "30f6b5daf8dda01c6103",
+    "url": "/dots/static/js/main.6761a2db.chunk.js"
+  },
+  {
+    "revision": "46fe54b079936b915069",
+    "url": "/dots/static/js/runtime~main.8e324884.js"
+  },
+  {
+    "revision": "21de6bb5995f5636e62b",
+    "url": "/dots/static/js/2.15ebd329.chunk.js"
+  },
+  {
+    "revision": "63712678462670b24e09048ca136027e",
+    "url": "/dots/index.html"
+  }
+];
